d75e2610d6063d3347d0f312192e453551d22f5d
sevenjobs <121744301+sevenjobs@users.noreply.github.com>
2023-03-12 21:34:22 +0800

    1.1

--- server/start.py
+++ server/start.py
@@ -1,2 +1,10 @@
-if __name__ == "__main__":
-    pass
+import socket
+import setting
+if __name__ == "__main__": 
+    s = socket.socket()
+    s.bind((setting.host, setting.port))
+    while True:
+        c,addr = s.accept()
+        data = c.recv()
+        c.send('test!')
+        c.close()
