9a5976c7035de21ccef66acf65de4bff8fb5c06c
sevenjobs <121744301+sevenjobs@users.noreply.github.com>
2023-03-12 21:40:45 +0800

    1.1.2

--- server/start.py
+++ server/start.py
@@ -1,10 +1,12 @@
 import socket
 import setting
+import processing
 if __name__ == "__main__": 
     s = socket.socket()
     s.bind((setting.host, setting.port))
     while True:
         c,addr = s.accept()
         data = c.recv()
+        processing.unified(data)
         c.send('test!')
         c.close()
